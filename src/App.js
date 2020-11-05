93b5defe124bc6e27947d67a2e81098031b9c1b8
FIXDISC <acavieres@hotmail.com>
2020-11-04 23:58:57 -0300

    Mod Solo Redux

--- src/App.js
+++ src/App.js
@@ -3,30 +3,7 @@ import React, {Component} from 'react'
 import ListaTareas from './components/ListaTareas/ListaTareas'
 //import AddTarea from './AddTarea'
 import AddTarea from './components/AddTarea/AddTarea'
-
 class  App extends Component {
-  state = {
-      tareas:[
-        {id: 1, nombre: 'Comprar Leche'},
-        {id: 2, nombre: 'Borar tareas' }
-      ]
-  }
-
-  deleteTarea = (id) => {
-    const tareas = this.state.tareas.filter(tarea=>{
-      return tarea.id !== id
-    })
-    this.setState({
-      tareas
-    })
-  }
-  addTarea = (tarea) => {
-    let tareas = [...this.state.tareas, tarea]
-    this.setState({
-      tareas
-    })
-  }
-
   render(){
     return (
       <div className="container">
